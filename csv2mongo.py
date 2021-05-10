36820aea1d54ca240da4049fb86cabd133c7358c
Muzaffer <71003444+muzafferbulut@users.noreply.github.com>
2021-05-11 00:15:14 +0300

    Add files via upload

---
+++ csv2mongo.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 17 15:41:55 2021
+
+@author: Muzaffer
+"""
+
+import pandas as pd
+from pymongo import MongoClient
+from os import listdir
+import json
+
+with open("info.json","r") as file:
+    JsonFile = json.load(file)
+
+client = MongoClient()
+database = client[JsonFile["database"]]
+collection = database[JsonFile["collection"]]
+
+files = listdir(JsonFile["filePath"])
+
+for file in range(len(files)):
+    fileName = JsonFile["filePath"]+"\\"+files[file]
+    csvFile = pd.read_csv(fileName)
+    
+    [x,y] = csvFile.shape
+    columns = csvFile.columns
+    data = csvFile.values
+    
+    for row in range(x):
+        
+        listColumns = list(columns)
+        dataRow = data[row]
+        
+        DataDict = dict(zip(listColumns, dataRow))
+        
+        collection.insert(DataDict)
+
+print("Data has been inserted")
